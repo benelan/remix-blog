1ee8ef5c7884656bf4946a793ae7aa2e9f6cf335
Ben Elan <benelan@protonmail.com>
2022-03-14 13:58:11 -0700

    fix: remove blog due to vercel path issue

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,5 +1,4 @@
 import TopNav from "./TopNav";
-import Socials from "./Socials";
 
 type LayoutProps = {
   children: JSX.Element;
@@ -7,14 +6,11 @@ type LayoutProps = {
 };
 
 const Layout = ({ children, theme }: LayoutProps) => (
-  <div id="wrapper" className={`wrapper flex flex-col ${theme} text-text`}>
+  <div id="wrapper" className={`wrapper flex flex-col ${theme} text-type`}>
     <TopNav theme={theme} />
     <main className="pt-16 px-8 flex-grow flex flex-col justify-center lg:px-16 bg-background">
       {children}
     </main>
-    <div className="bg-background">
-    <Socials />
-    </div>
   </div>
 );
 
